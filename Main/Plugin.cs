165b1dfe98ecc991ddf9d0f8ae215db8a3d45a50
BrainyHedgehog <BrainyHedgehog@users.noreply.github.com>
2020-11-04 23:11:57 +0300

    Added Event_OnPostLevelLoaded

--- Main/Plugin.cs
+++ Main/Plugin.cs
@@ -1,5 +1,8 @@
-﻿using Rocket.Core.Plugins;
-using DevIncModule;
+﻿using DevIncModule;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Terminals
 {
@@ -12,12 +15,31 @@ namespace Terminals
         {
             DIMessager.PluginInfoStatus(this);
             Instance = this;
+            Level.onPostLevelLoaded += Event_OnPostLevelLoaded;
         }
 
         protected override void Unload()
         {
             DIMessager.PluginInfoStatus(this);
             Instance = null;
+            Level.onPostLevelLoaded -= Event_OnPostLevelLoaded;
+        }
+
+        private void Event_OnPostLevelLoaded(int level)
+        {
+            foreach (Transform transform in LevelObjects.models)
+            {
+                var ID = ushort.Parse(transform.gameObject.name);
+
+                if (ID == Configuration.Instance.GroceryTerminalID || ID == Configuration.Instance.OrderingTerminalID)
+                {
+                    Storage storage = ID == Configuration.Instance.GroceryTerminalID ? new Storage(new List<StoredItem>(Configuration.Instance.standardGroceryItems)) : new Storage(new List<StoredItem>(Configuration.Instance.standardOrderingItems), new List<PlayerShoppingBasket>());
+                    var newTerminal = new Terminal(transform.position, storage, new Error());
+                    Configuration.Instance.terminals.Add(newTerminal);
+                }
+            }
+
+            Configuration.Save();
         }
     }
 }
