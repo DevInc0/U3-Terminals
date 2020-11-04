52b4bba2d4affca8656f0cc6aae320902a49d92a
BrainyHedgehog <BrainyHedgehog@users.noreply.github.com>
2020-11-04 23:12:54 +0300

    Added terminals information

--- Main/Config.cs
+++ Main/Config.cs
@@ -5,7 +5,14 @@ namespace Terminals
 {
     public class Config : IRocketPluginConfiguration
     {
+
+        public ushort GroceryTerminalID, OrderingTerminalID;
+
+        public List<Terminal> terminals;
+
         public List<Error> errors;
+
+        public List<StoredItem> standardGroceryItems, standardOrderingItems;
 
         public void LoadDefaults()
         {
@@ -15,6 +22,21 @@ namespace Terminals
                 new Error("#()% ERROR ||!@   EINTR | Interrupted system call", "gen_ser connect *|Main| -res -rel -d", 600f),
                 new Error("#_001 ERROR |!!*!@   EIO | Input/output error", "*|Main| -res -rel -d", 900f)
             };
+
+            standardGroceryItems = new List<StoredItem>
+            {
+                new StoredItem { ID = 81, amount = 2 },
+                new StoredItem { ID = 15, amount = 3 }
+            };
+
+            standardOrderingItems = new List<StoredItem>
+            {
+                new StoredItem { ID = 253, amount = 4 },
+                new StoredItem { ID = 269, amount = 1 }
+            };
+
+            GroceryTerminalID = 0;
+            OrderingTerminalID = 0;
         }
     }
 }
