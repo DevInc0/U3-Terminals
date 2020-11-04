e8fb6f0e4ea6dd7741649fce1ed5e81f6542e143
BrainyHedgehog <BrainyHedgehog@users.noreply.github.com>
2020-11-04 23:16:55 +0300

    Added DisplayTerminalItems() and ReloadTerminal() for future features

--- Structures/Terminal.cs
+++ Structures/Terminal.cs
@@ -2,46 +2,60 @@
 using Rocket.Unturned.Player;
 using SDG.Unturned;
 using Steamworks;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Terminals.Structures
+namespace Terminals
 {
     public struct Terminal
     {
         public Vector3 position;
 
+        public Storage storage;
+
         public Error error;
 
-        public Storage storage;
+        public Terminal(Vector3 _position, Storage _storage, Error _error)
+        {
+            position = _position;
+            storage = _storage;
+            error = _error;
+        }
 
         public bool isReloading
         {
             get
             {
-                return error.reloadingTime != 0f; 
+                return error.reloadingTime != 0f;
             }
         }
 
         public void OpenTerminal(CSteamID steamID)
         {
-            if(isReloading)
+            if (isReloading)
                 return;
 
             UnturnedPlayer.FromCSteamID(steamID).Player.setPluginWidgetFlag(EPluginWidgetFlags.Modal, true);
 
-            if(storage.storedItems.TrueForAll(item => item.amount == 0))
+            if (storage.storedItems.TrueForAll(item => item.amount == 0))
             {
                 error = Error.GetRandomError();
                 EffectManager.sendUIEffect((ushort)EUIs.DEBUG_TERMINAL, 1000, steamID, true, error.warningMessage);
                 return;
             }
 
-            DisplayTerminalItems(steamID);            
+            DisplayTerminalItems(steamID);
+        }
+
+        public void ReloadTerminal()
+        {
+            error = new Error();
+
+            storage.storedItems = new List<StoredItem>(storage.baskets == null ? Plugin.Instance.Configuration.Instance.standardGroceryItems : Plugin.Instance.Configuration.Instance.standardOrderingItems);
+
+            if (storage.baskets != null) storage.baskets.Clear();
+
+            Plugin.Instance.Configuration.Save();
         }
 
         public void DisplayTerminalItems(CSteamID steamID)
@@ -50,11 +64,13 @@ namespace Terminals.Structures
 
             EffectManager.sendUIEffect(id, 1000, steamID, true);
 
-            foreach(StoredItem storedItem in storage.storedItems)
+            byte index = 0;
+            foreach (StoredItem storedItem in storage.storedItems)
             {
-                if(DIFinder.GetItemAsset(storedItem.ID, out ItemAsset itemAsset))
+                if (DIFinder.GetItemAsset(storedItem.ID, out ItemAsset itemAsset))
                 {
-                    EffectManager.sendUIEffectText(1000, steamID, true, itemAsset.name.Trim(), $"{itemAsset.name.Trim()} \n {storedItem.amount}");
+                    EffectManager.sendUIEffectText(1000, steamID, true, $"Box_{index}", $"{itemAsset.name.Trim()} \n {storedItem.amount}");
+                    index++;
                 }
             }
         }
